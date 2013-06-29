6c2a4dd5a3e127da93516ff29d955118107e0766
khobaib <khobaib@gmail.com>
2013-06-30 02:29:56 +0600

    alertdialog window leaks bug fixed.

--- SmartyPlant/src/com/mistersmartyplants/core/Submit.java
+++ SmartyPlant/src/com/mistersmartyplants/core/Submit.java
@@ -32,185 +32,177 @@ import com.mistersmartyplants.utility.GlobalState;
 import com.mistersmartyplants.utility.SmartyPlantApplication;
 
 public class Submit extends Activity {
-	Context mContext = this;
-	String country;
-	EditText countryField;
+    Context mContext = this;
+    String country;
+    EditText countryField;
 
-	String state;
-	EditText stateField;
+    String state;
+    EditText stateField;
 
-	String city;
-	EditText cityField;
+    String city;
+    EditText cityField;
 
-	String region;
-	EditText regionField;
+    String region;
+    EditText regionField;
 
-	String desc;
-	EditText descField;
-	ProgressDialog dialog = null;
-	boolean requestResult = false;
-	JsonParser jsonParser;
-	SmartyPlantApplication appInstance;
+    String desc;
+    EditText descField;
+    ProgressDialog dialog = null;
+    boolean requestResult = false;
+    JsonParser jsonParser;
+    SmartyPlantApplication appInstance;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		BugSenseHandler.initAndStartSession(mContext, "f2391cbb");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        BugSenseHandler.initAndStartSession(mContext, "f2391cbb");
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.submit_form);
-		jsonParser = new JsonParser();
-		appInstance = (SmartyPlantApplication)getApplication();
-		countryField = (EditText) findViewById(R.id.country_field);
-		stateField = (EditText) findViewById(R.id.state_field);
-		cityField = (EditText) findViewById(R.id.city_field);
-		regionField = (EditText) findViewById(R.id.region_field);
-		descField = (EditText) findViewById(R.id.desc_field);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submit_form);
+        jsonParser = new JsonParser();
+        appInstance = (SmartyPlantApplication)getApplication();
+        countryField = (EditText) findViewById(R.id.country_field);
+        stateField = (EditText) findViewById(R.id.state_field);
+        cityField = (EditText) findViewById(R.id.city_field);
+        regionField = (EditText) findViewById(R.id.region_field);
+        descField = (EditText) findViewById(R.id.desc_field);
 
-		Gallery gallery = (Gallery) findViewById(R.id.gallery_view);
-		GalleryAdapter gAdapter = new GalleryAdapter(mContext, 2);
-		gallery.setAdapter(gAdapter);
+        Gallery gallery = (Gallery) findViewById(R.id.gallery_view);
+        GalleryAdapter gAdapter = new GalleryAdapter(mContext, 2);
+        gallery.setAdapter(gAdapter);
 
-		Button submit = (Button) findViewById(R.id.submit);
-		submit.setOnClickListener(new OnClickListener() {
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View arg0) {
+            @Override
+            public void onClick(View arg0) {
 
-				country = countryField.getEditableText().toString();
-				if (country.equalsIgnoreCase("") || country == null) {
-					Toast.makeText(mContext,
-							"Country field can not be left empty", 3000).show();
-				} else {
-					state = stateField.getEditableText().toString();
-					city = cityField.getEditableText().toString();
-					region = regionField.getEditableText().toString();
-					desc = descField.getEditableText().toString();
-					UploadTask task = new UploadTask();
-					task.execute();
-				}
-			}
-		});
-	}
+                country = countryField.getEditableText().toString();
+                if (country.equalsIgnoreCase("") || country == null) {
+                    Toast.makeText(mContext,
+                            "Country field can not be left empty", 3000).show();
+                } else {
+                    state = stateField.getEditableText().toString();
+                    city = cityField.getEditableText().toString();
+                    region = regionField.getEditableText().toString();
+                    desc = descField.getEditableText().toString();
+                    UploadTask task = new UploadTask();
+                    task.execute();
+                }
+            }
+        });
+    }
 
-	// ============= Background Task ========
-	class UploadTask extends AsyncTask<Void, Void, JSONObject> {
-		ProgressDialog dialog = null;
+    // ============= Background Task ========
+    class UploadTask extends AsyncTask<Void, Void, JSONObject> {
+        ProgressDialog dialog = null;
 
-		// alertDialog = null;
+        // alertDialog = null;
 
-		@Override
-		protected void onPreExecute() {
-			dialog = new ProgressDialog(mContext);
-			dialog.setTitle(" ");
-			dialog.setIcon(R.drawable.logo);
-			dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-			dialog.setCancelable(false);
-			dialog.setMessage("Uploading Mystery Plant");
-			dialog.show();
-		}
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(mContext);
+            dialog.setTitle(" ");
+            dialog.setIcon(R.drawable.logo);
+            dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            dialog.setCancelable(false);
+            dialog.setMessage("Uploading Mystery Plant");
+            dialog.show();
+        }
 
-		@Override
-		protected JSONObject doInBackground(Void... params) {
-			try {
-				String url = Constants.METHOD_PLANT;
-				
-				
-				JSONArray arr = new JSONArray();
-				for (int i = 0; i < GlobalState.getInstance().currentBitmaps.size(); i++) {
-					JSONObject json = new JSONObject();
-					ByteArrayOutputStream baos = new ByteArrayOutputStream();
-					
-					String path = GlobalState.getInstance().currentBitmaps.get(i);
-					Bitmap bm = GlobalState.getInstance().bitmapFromPath(path);
-					bm.compress(Bitmap.CompressFormat.JPEG, 50, baos);
-					// bm.recycle();
-					byte[] b = baos.toByteArray();
-					baos.close();
-					baos = null;
+        @Override
+        protected JSONObject doInBackground(Void... params) {
+            try {
+                String url = Constants.METHOD_PLANT;
 
-					String base64 = Base64.encodeToString(b, Base64.DEFAULT);
-					json.put("base64String", base64.replaceAll("\n", ""));
-					json.put("description", desc);
-					json.put("country", country);
-					json.put("state", state);
-					json.put("city", city);
-					json.put("region", region);
-					arr.put(json);
-				}
-				String loginData = arr.toString();
-				ServerResponse response = jsonParser
-						.retrieveServerData(1, Constants.REQUEST_TYPE_POST,
-								url, null, loginData, appInstance.getAccessToken());
-				
-				Log.d("vote", ""+ response.getStatus());
-				Log.d("vote", response.getjObj().toString());
-				
 
-				if (response.getStatus() == 200 || response.getStatus() == 201) {
-					return response.getjObj();
-				} else {
-					return null;
-				}
-				
-//				result = DataConnector.getInstance().uploadImage(country,
-//						state, city, region, desc);
-		//		Log.d("<<<<>>>>", "result = " + result);
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}
+                JSONArray arr = new JSONArray();
+                for (int i = 0; i < GlobalState.getInstance().currentBitmaps.size(); i++) {
+                    JSONObject json = new JSONObject();
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-		}
+                    String path = GlobalState.getInstance().currentBitmaps.get(i);
+                    Bitmap bm = GlobalState.getInstance().bitmapFromPath(path);
+                    bm.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+                    // bm.recycle();
+                    byte[] b = baos.toByteArray();
+                    baos.close();
+                    baos = null;
 
-		protected void onPostExecute(JSONObject result) {
-			try {
-				dialog.dismiss();
-			} catch (Exception e) {
+                    String base64 = Base64.encodeToString(b, Base64.DEFAULT);
+                    json.put("base64String", base64.replaceAll("\n", ""));
+                    json.put("description", desc);
+                    json.put("country", country);
+                    json.put("state", state);
+                    json.put("city", city);
+                    json.put("region", region);
+                    arr.put(json);
+                }
+                String loginData = arr.toString();
+                ServerResponse response = jsonParser
+                        .retrieveServerData(1, Constants.REQUEST_TYPE_POST,
+                                url, null, loginData, appInstance.getAccessToken());
 
-			}
+                Log.d("vote", ""+ response.getStatus());
+                Log.d("vote", response.getjObj().toString());
+
+
+                if (response.getStatus() == 200 || response.getStatus() == 201) {
+                    return response.getjObj();
+                } else {
+                    return null;
+                }
+
+                //				result = DataConnector.getInstance().uploadImage(country,
+                //						state, city, region, desc);
+                //		Log.d("<<<<>>>>", "result = " + result);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        protected void onPostExecute(JSONObject result) {
+            try {
+                dialog.dismiss();
+            } catch (Exception e) {
+
+            }
             Log.d("OnPostExec", "res: "+result);
+            String msg = result.optString("response");
 
-			AlertDialog.Builder alertdialog = new AlertDialog.Builder(mContext);
-			alertdialog.setIcon(R.drawable.logo);
-			alertdialog.setTitle(" ");
-			String msg = result.optString("response");
-			
-			if (msg.equalsIgnoreCase("success")) {
-				alertdialog.setMessage("Plant Submitted Successfully");
-				requestResult = true;
-			}
+            if (msg.equalsIgnoreCase("success")) {
+                alert("Plant Submitted Successfully.", true);
+            }
 
-			else {
-				alertdialog.setMessage("Failed to Submit Plant");
-				requestResult = false;
-			}
-			alertdialog.setPositiveButton("Ok",
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog, int which) {
-							try{
-							dialog.dismiss();
-							}
-							catch(Exception e ){
-								
-							}							finish();
-							if (requestResult) {
-								GlobalState.getInstance().currentBitmaps.clear();	
-								startActivity(new Intent(mContext,
-										HomeScreen.class));
-							}
-						}
-					});
-			alertdialog.setCancelable(true);
-			alertdialog.create().show();
+            else {
+                alert("Failed to Submit Plant.", false);
+            }
+        };
+    }
 
-			if (msg.equalsIgnoreCase("success")) {
-				GlobalState.getInstance().currentBitmaps.clear();
-				finish();
-				Intent i = new Intent(Submit.this, HomeScreen.class);
-				i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-				startActivity(i);
-			}
-		};
-	}
+
+
+    void alert(String message, final Boolean success) {
+        AlertDialog.Builder bld = new AlertDialog.Builder(Submit.this);
+        bld.setIcon(R.drawable.logo);
+        bld.setMessage(message);
+        bld.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if(success){
+                    GlobalState.getInstance().currentBitmaps.clear();
+                    finish();
+                    Intent i = new Intent(Submit.this, HomeScreen.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+
+            }
+        });
+        bld.create().show();
+    }
 
 }
