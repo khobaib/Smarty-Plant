1d946c990e2773fbb615a6265c1d14aebd66c81d
administrator <administrator@administrator.(none)>
2013-07-19 10:29:46 +0200

    Twitter login flow

--- SmartyPlant/src/com/mistersmartyplants/utility/Constants.java
+++ SmartyPlant/src/com/mistersmartyplants/utility/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
     public static final String PREF_KEY_OAUTH_TOKEN = "oauth_token";
     public static final String PREF_KEY_OAUTH_SECRET = "oauth_token_secret";
     public static final String PREF_KEY_TWITTER_LOGIN = "isTwitterLogedIn";
-    public static final String TWITTER_CALLBACK_URL = "oauth://t4jsample";
+    public static final String TWITTER_CALLBACK_URL = "oauth://smartyplants";
     public static final String URL_TWITTER_AUTH = "auth_url";
     public static final String URL_TWITTER_OAUTH_VERIFIER = "oauth_verifier";
     public static final String URL_TWITTER_OAUTH_TOKEN = "oauth_token";
